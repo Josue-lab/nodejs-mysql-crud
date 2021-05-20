9d991c07e1e1ffb8ceacadd0b77603a4921dfca5
Josue-lab <58716020+Josue-lab@users.noreply.github.com>
2021-05-20 00:06:19 -0500

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -2,8 +2,8 @@ module.exports = {
   mysql: {
     host: 'paystand-mysql',
     port: 3306,
-    user: 'paystand',
-    password: 'paystand',
+    user: 'root',
+    password: 'admin',
     database: 'nodejs2'
   },
   app: {
